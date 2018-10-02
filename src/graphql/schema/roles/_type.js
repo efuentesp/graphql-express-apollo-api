4a1915227e29d86acd9bad9e1a805fbf722a4b3c
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2018-10-02 08:42:48 -0500

    Intento por resolver las promesas

--- src/graphql/schema/roles/_type.js
+++ src/graphql/schema/roles/_type.js
@@ -25,10 +25,11 @@ export const typeResolvers = {
     permissions(parent, args) {
       return axios.get(config.jsonServerUrl + '/permissions-assigned?roleId=' + parent.id)
       .then(res => {
-        var permissions
-        res.data.forEach(async (value, key) => {
-          const permission = await axios.get(config.jsonServerUrl + '/permission/' + value.permissionId)
-          console.log(await permission.json)
+        var permissions = []
+        res.data.forEach((value, key) => {
+          permissions = axios.get(config.jsonServerUrl + '/permission/' + value.permissionId)
+            .then(res => res.data)
+          console.log(permissions)
         })
         return permissions
       }) 
